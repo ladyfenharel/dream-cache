2cdcc98a674e6c987326ea8e72f257a5fc455592
Amanda Stone <amandastonexo@gmail.com>
2025-07-11 21:05:45 -0400

    Initial commit

---
+++ src/components/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Detail = () => {
+    return (
+        <div className="p-4">
+        <h1 className="text-2xl font-bold">Detail</h1>
+        <p>Here you can find detailed information about your dreams.</p>
+        </div>
+    );
+};
+
+export default Detail;
---
+++ src/components/Journal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Journal = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Journal</h1>
+      <p>Keep track of your thoughts and experiences.</p>
+    </div>
+  );
+};
+
+export default Journal;
